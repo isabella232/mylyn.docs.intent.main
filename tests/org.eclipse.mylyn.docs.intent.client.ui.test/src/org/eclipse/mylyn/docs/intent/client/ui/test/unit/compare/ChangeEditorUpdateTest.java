b435eef4639541bb0a96703a4f6191a19d14d05e
alagarde <alex.lagarde@obeo.fr>
2011-09-02 15:16:36 +0200

    [Test UI] adding emf compare use cases & adding a new identified issue

--- tests/org.eclipse.mylyn.docs.intent.client.ui.test/src/org/eclipse/mylyn/docs/intent/client/ui/test/unit/compare/ChangeEditorUpdateTest.java
+++ tests/org.eclipse.mylyn.docs.intent.client.ui.test/src/org/eclipse/mylyn/docs/intent/client/ui/test/unit/compare/ChangeEditorUpdateTest.java
@@ -25,17 +25,27 @@ import org.eclipse.mylyn.docs.intent.core.document.IntentSection;
  */
 public class ChangeEditorUpdateTest extends AbstractUITest {
 
-	private static final String PREFIX_STRING_1 = "* an element defined in the right model is not matching any element in the left model (added element)";
+	private static final String FAILURE_MESSAGE = "Editor update dit not occur has expected";
 
-	private static final String PREFIX_STRING_2 = "Otherwise, it is a deleted element.";
+	private static String INTENT_DOCUMENT_EXAMPLE_PATH = "data/unit/documents/editorupdates/changeEditorUpdateTest.intent";
 
-	private static final String NEW_SECTION_1 = "\n\tSection {\n\t\t\tSubSection1\n\n\t\tMySubSection\n\t}";
+	private static final String PREFIX_SECTION_1_1 = "* an element defined in the right model is not matching any element in the left model (added element)";
 
-	private static final String NEW_SECTION_2 = "\n\tSection {\n\t\t\tSubSection2\n\n\t\tMySubSection\n\t}";
+	private static final String PREFIX_SECTION_1_2 = "Otherwise, it is a deleted element.";
+
+	private static final String NEW_SECTION_1 = "\n\tSection {\n\t\tSubSection1\n\n\t\tMySubSection\n\t}";
+
+	private static final String NEW_SECTION_2 = "\n\tSection {\n\t\tSubSection2\n\n\t\tMySubSection\n\t}";
+
+	private static final String PREFIX_CHAPTER_1 = "Document {\n\tChapter {";
+
+	private static final String PREFIX_CHAPTER_2 = "\t}\n\tChapter {";
 
 	private IntentSection section;
 
 	private IntentEditor editor;
+
+	private IntentEditorDocument document;
 
 	/**
 	 * {@inheritDoc}
@@ -45,31 +55,85 @@ public class ChangeEditorUpdateTest extends AbstractUITest {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		setUpIntentProject("intentProject", "data/unit/documents/editorupdates/changeEditorUpdateTest.intent");
+		setUpIntentProject("intentProject", INTENT_DOCUMENT_EXAMPLE_PATH);
 		section = getIntentDocument().getChapters().iterator().next().getSubSections().iterator().next();
-		editor = openIntentEditor(section);
+
+	}
+
+	/**
+	 * Ensures that, when adding a new title to a chapter, when emf compare is used to update the repository
+	 * the chapter keeps its expected location.
+	 */
+	public void testAddTitleToExistingChapter() {
+		// Step 1 : opening an editor on the document
+		editor = openIntentEditor();
+		document = (IntentEditorDocument)editor.getDocumentProvider().getDocument(editor.getEditorInput());
+
+		// Step 2 : update section by adding 2 subsections textually
+
+		String documentContent = document.get();
+		String expectedDocumentContent = documentContent.replace(PREFIX_CHAPTER_1, PREFIX_CHAPTER_1
+				+ "\n\t\tMy Chapter Title");
+		document.set(expectedDocumentContent);
+
+		// Step 3 : save editor
+		editor.doSave(new NullProgressMonitor());
+		waitForAllOperationsInUIThread();
+
+		// Step 4 : checking that when resierializing the parsed document we obtain the expected text
+		String newDocumentContent = document.get();
+		assertEquals(FAILURE_MESSAGE, expectedDocumentContent, newDocumentContent);
+	}
+
+	/**
+	 * Ensures that, when adding a new parapraph to a chapter containing no description unit, when emf compare
+	 * is used to update the repository the chapter keeps its expected location and structure.
+	 */
+	public void testAddParagraphToChapterWithoutDescriptionUnits() {
+		// Step 1 : opening an editor on the document
+		editor = openIntentEditor();
+		document = (IntentEditorDocument)editor.getDocumentProvider().getDocument(editor.getEditorInput());
+
+		// Step 2 : update section by adding 2 subsections textually
+
+		String documentContent = document.get();
+		String expectedDocumentContent = documentContent.replace(PREFIX_CHAPTER_2, PREFIX_CHAPTER_2
+				+ "\n\n\t\tA new description Unit");
+		document.set(expectedDocumentContent);
+
+		// Step 3 : save editor
+		editor.doSave(new NullProgressMonitor());
+		waitForAllOperationsInUIThread();
+
+		// Step 4 : checking that when resierializing the parsed document we obtain the expected text
+		String newDocumentContent = document.get();
+		assertEquals(FAILURE_MESSAGE, expectedDocumentContent, newDocumentContent);
 	}
 
 	/**
 	 * Ensures that when typing new sections inside the Intent Document, when emf compare is used to update
 	 * the repository the structure is respected.
 	 */
-	public void testSectionOrder() {
-		IntentEditorDocument document = (IntentEditorDocument)editor.getDocumentProvider().getDocument(
-				editor.getEditorInput());
+	public void testSubSectionOrder() {
+		// Step 1 : opening an editor on a section
+		editor = openIntentEditor(section);
+		document = (IntentEditorDocument)editor.getDocumentProvider().getDocument(editor.getEditorInput());
 
+		// Step 2 : update section by adding 2 subsections textually
 		String documentContent = document.get();
-		String expectedDocumentContent = documentContent.replace(PREFIX_STRING_1, PREFIX_STRING_1
+		String expectedDocumentContent = documentContent.replace(PREFIX_SECTION_1_1, PREFIX_SECTION_1_1
 				+ NEW_SECTION_1);
-		expectedDocumentContent = expectedDocumentContent.replace(PREFIX_STRING_2, PREFIX_STRING_2
+		expectedDocumentContent = expectedDocumentContent.replace(PREFIX_SECTION_1_2, PREFIX_SECTION_1_2
 				+ NEW_SECTION_2);
 		document.set(expectedDocumentContent);
+
+		// Step 3 : save editor
 		editor.doSave(new NullProgressMonitor());
 		waitForAllOperationsInUIThread();
 
+		// Step 4 : checking that when resierializing the parsed section we obtain the expected text
 		String newDocumentContent = document.get();
-		assertEquals("Editor update dit not occur has expected", expectedDocumentContent, newDocumentContent);
-
+		assertEquals(FAILURE_MESSAGE, expectedDocumentContent, newDocumentContent);
 	}
 
 	/**
