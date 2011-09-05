54d4f1be0e970ff259bef569ea9cd1aad40ffeef
wpiers <william.piers@obeo.fr>
2011-09-05 17:23:50 +0200

    fixed naming

--- tests/org.eclipse.mylyn.docs.intent.client.ui.test/src/org/eclipse/mylyn/docs/intent/client/ui/test/unit/demo/compilation/CompileTest.java
+++ tests/org.eclipse.mylyn.docs.intent.client.ui.test/src/org/eclipse/mylyn/docs/intent/client/ui/test/unit/demo/compilation/CompileTest.java
@@ -29,11 +29,11 @@ public class CompileTest extends AbstractDemoTest {
 
 	private static final String COMPILATION_ERROR_MESSAGE = "The feature nsPrefixx doesn't exists in EPackage";
 
-	private static final String WARNING_TEXT_PATTERN = "nsURI = \"\";";
+	private static final String INFOS_TEXT_PATTERN = "nsURI = \"\";";
 
-	private static final String NO_WARNING_TEXT_PATTERN = "nsURI = \"http://www.eclipse.org/emf/compare/match/1.1\";";
+	private static final String NO_INFOS_TEXT_PATTERN = "nsURI = \"http://www.eclipse.org/emf/compare/match/1.1\";";
 
-	private static final String COMPILATION_WARNING_MESSAGE = "-The namespace URI '' is not well formed";
+	private static final String COMPILATION_INFO_MESSAGE = "-The namespace URI '' is not well formed";
 
 	private IntentEditor editor;
 
@@ -65,43 +65,45 @@ public class CompileTest extends AbstractDemoTest {
 		editor.doSave(new NullProgressMonitor());
 		waitForAllOperationsInUIThread();
 
-		// Step 5 : ensure that the compilation error has been detected
+		// Step 5 : ensure that the compilation error no longer exists
 		assertEquals(
 				false,
 				hasIntentAnnotation(editor, IntentAnnotationMessageType.COMPILER_ERROR,
 						COMPILATION_ERROR_MESSAGE, true));
 	}
 
+	// TODO test compilation warnings ?
+
 	/**
-	 * Ensures that compilation warnings are detected and can be fixed.
+	 * Ensures that compilation infos are detected and can be fixed.
 	 */
-	public void testCompilationWarnings() {
+	public void testCompilationInfos() {
 		// Step 1 : opening an editor on the document
 		editor = openIntentEditor(getIntentDocument().getChapters().get(3).getSubSections().get(0));
 		document = (IntentEditorDocument)editor.getDocumentProvider().getDocument(editor.getEditorInput());
 
 		// Step 2 : update section by adding incorrect content
 		String initialContent = document.get();
-		String newContent = initialContent.replaceFirst(NO_WARNING_TEXT_PATTERN, WARNING_TEXT_PATTERN);
+		String newContent = initialContent.replaceFirst(NO_INFOS_TEXT_PATTERN, INFOS_TEXT_PATTERN);
 		document.set(newContent);
 		editor.doSave(new NullProgressMonitor());
 		waitForAllOperationsInUIThread();
 
-		// Step 3 : ensure that the compilation warning has been detected
+		// Step 3 : ensure that the compilation info has been detected
 		assertEquals(
 				true,
 				hasIntentAnnotation(editor, IntentAnnotationMessageType.COMPILER_INFO,
-						COMPILATION_WARNING_MESSAGE, true));
+						COMPILATION_INFO_MESSAGE, true));
 
-		// Step 4 : fix the warning by resetting the content
+		// Step 4 : fix the info by resetting the content
 		document.set(initialContent);
 		editor.doSave(new NullProgressMonitor());
 		waitForAllOperationsInUIThread();
 
-		// Step 5 : ensure that the compilation warning has been detected
+		// Step 5 : ensure that the compilation info no longer exists
 		assertEquals(
 				false,
-				hasIntentAnnotation(editor, IntentAnnotationMessageType.COMPILER_WARNING,
-						COMPILATION_WARNING_MESSAGE, true));
+				hasIntentAnnotation(editor, IntentAnnotationMessageType.COMPILER_INFO,
+						COMPILATION_INFO_MESSAGE, true));
 	}
 }
