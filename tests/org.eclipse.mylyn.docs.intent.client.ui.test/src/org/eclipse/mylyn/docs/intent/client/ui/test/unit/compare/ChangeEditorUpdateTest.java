a0930e7ddb3a43514b63d79044f5d311304a2481
alagarde <alex.lagarde@obeo.fr>
2011-09-16 14:24:00 +0200

    [UI Tests] adding tests case on EMFCompare

--- tests/org.eclipse.mylyn.docs.intent.client.ui.test/src/org/eclipse/mylyn/docs/intent/client/ui/test/unit/compare/ChangeEditorUpdateTest.java
+++ tests/org.eclipse.mylyn.docs.intent.client.ui.test/src/org/eclipse/mylyn/docs/intent/client/ui/test/unit/compare/ChangeEditorUpdateTest.java
@@ -25,6 +25,12 @@ import org.eclipse.mylyn.docs.intent.core.document.IntentSection;
  */
 public class ChangeEditorUpdateTest extends AbstractIntentUITest {
 
+	private static final int FOCUS_ON_DOCUMENT = 3;
+
+	private static final String SAMPLE_MODELING_CONTENT = "\n\t\n\t@M\n\t\tSide {\n\t\t\tserializable = \"true\";\n\t\t}\n\tM@\n\t\n\n\t";
+
+	private static final String MIDDLE_OF_DESCRIPTION_UNIT = "@see \"TheUnmatchElementConcept\" ). T";
+
 	private static final String A_NEW_DESCRIPTION_UNIT = "\n\n\tA new description Unit";
 
 	private static final String LINEBREAK_AND_INDENT = "\n\t";
@@ -65,28 +71,6 @@ public class ChangeEditorUpdateTest extends AbstractIntentUITest {
 	}
 
 	/**
-	 * Ensures that when typing new sections inside the Intent Document, when emf compare is used to update
-	 * the repository the structure is respected.
-	 */
-	public void testSubSectionOrderWithFocusOnDocument() {
-		// opening an editor on a section
-		editor = openIntentEditor();
-
-		genericTestSubSectionOrderWithFocusOnDocuments(3);
-	}
-
-	/**
-	 * Ensures that when typing new sections inside the Intent Document, when emf compare is used to update
-	 * the repository the structure is respected.
-	 */
-	public void testSubSectionOrderWithFocusOnSection() {
-		// opening an editor on a section
-		editor = openIntentEditor(section);
-
-		genericTestSubSectionOrderWithFocusOnDocuments(1);
-	}
-
-	/**
 	 * Ensures that, when adding a new paragraph to a chapter containing no description unit, when emf compare
 	 * is used to update the repository the chapter keeps its expected location and structure.
 	 */
@@ -94,7 +78,7 @@ public class ChangeEditorUpdateTest extends AbstractIntentUITest {
 		// opening an editor on the second chapter
 		editor = openIntentEditor(getIntentChapter(2));
 
-		genericTestAddParagraphToChapterWithoutDescriptionUnits("Chapter {", A_NEW_DESCRIPTION_UNIT);
+		genericUpdateTest("Chapter {", A_NEW_DESCRIPTION_UNIT);
 	}
 
 	/**
@@ -105,8 +89,7 @@ public class ChangeEditorUpdateTest extends AbstractIntentUITest {
 		// opening an editor on the second chapter
 		editor = openIntentEditor(getIntentChapter(2));
 
-		genericTestAddParagraphToChapterWithoutDescriptionUnits("The 2.1 Section.\n\t}",
-				A_NEW_DESCRIPTION_UNIT);
+		genericUpdateTest("The 2.1 Section.\n\t}", A_NEW_DESCRIPTION_UNIT);
 	}
 
 	/**
@@ -117,8 +100,7 @@ public class ChangeEditorUpdateTest extends AbstractIntentUITest {
 		// opening an editor on the second chapter
 		editor = openIntentEditor(getIntentChapter(2));
 
-		genericTestAddParagraphToChapterWithoutDescriptionUnits("The 2.2 Section.\n\t}",
-				A_NEW_DESCRIPTION_UNIT);
+		genericUpdateTest("The 2.2 Section.\n\t}", A_NEW_DESCRIPTION_UNIT);
 	}
 
 	/**
@@ -149,30 +131,119 @@ public class ChangeEditorUpdateTest extends AbstractIntentUITest {
 		// opening an editor on the document
 		editor = openIntentEditor();
 
-		genericTestAddParagraphToChapterWithoutDescriptionUnits(PREFIX_CHAPTER_2_FOCUS_DOCUMENT,
-				"\n\n\t\tA new description Unit");
+		genericUpdateTest(PREFIX_CHAPTER_2_FOCUS_DOCUMENT, "\n\n\t\tA new description Unit");
 	}
 
 	/**
-	 * Ensures that, when adding a new paragraph to a chapter containing no description unit, when emf compare
-	 * is used to update the repository the chapter keeps its expected location and structure.
+	 * Ensures that, when adding a new modeling unit in the middle of an existing description unit, when emf
+	 * compare is used to update the repository the chapter keeps its expected location and structure.
+	 */
+	public void testAddModelingUnitBySplittingADescriptionUnitWithFocusOnDocument() {
+		// opening an editor on the document
+		editor = openIntentEditor();
+
+		genericUpdateTest(MIDDLE_OF_DESCRIPTION_UNIT, SAMPLE_MODELING_CONTENT, FOCUS_ON_DOCUMENT);
+	}
+
+	/**
+	 * Ensures that, when adding a new modeling unit in the middle of an existing description unit, when emf
+	 * compare is used to update the repository the chapter keeps its expected location and structure.
+	 */
+	public void testAddModelingUnitBySplittingADescriptionUnitWithFocusOnChapter() {
+		// opening an editor on the chapter
+		editor = openIntentEditor(getIntentChapter(1));
+
+		genericUpdateTest(MIDDLE_OF_DESCRIPTION_UNIT, SAMPLE_MODELING_CONTENT, 2);
+	}
+
+	/**
+	 * Ensures that, when adding a new modeling unit in the middle of an existing description unit, when emf
+	 * compare is used to update the repository the chapter keeps its expected location and structure.
+	 */
+	public void testAddModelingUnitBySplittingADescriptionUnitWithFocusOnSection() {
+		// opening an editor on the section
+		editor = openIntentEditor(getIntentSection(1, 1));
+
+		genericUpdateTest(MIDDLE_OF_DESCRIPTION_UNIT, SAMPLE_MODELING_CONTENT, 1);
+	}
+
+	/**
+	 * Ensures that when typing new sections inside the Intent Document, when emf compare is used to update
+	 * the repository the structure is respected.
+	 */
+	public void testSubSectionOrderWithFocusOnDocument() {
+		// opening an editor on a section
+		editor = openIntentEditor();
+
+		genericTestSubSectionOrderWithFocusOnDocuments(3);
+	}
+
+	/**
+	 * Ensures that when typing new sections inside the Intent Document, when emf compare is used to update
+	 * the repository the structure is respected.
+	 */
+	public void testSubSectionOrderWithFocusOnSection() {
+		// opening an editor on a section
+		editor = openIntentEditor(section);
+
+		genericTestSubSectionOrderWithFocusOnDocuments(1);
+	}
+
+	/**
+	 * <ul>
+	 * <li>Replaces the given prefix by prefix + contentToAdd in the current Document</li>
+	 * <li>Saves the editor</li>
+	 * <li>after merging with the repository content and serializing, check that the editor now displays the
+	 * expected document content.
+	 * <li>
+	 * </ul>
 	 * 
-	 * @param newDescriptionUnit
-	 *            the string corresponding to the new description unit to add
+	 * @param contentToAdd
+	 *            the string corresponding to the new content to add
 	 * @param prefix
 	 *            the prefix to use for identifying the chapter to modify
 	 */
-	private void genericTestAddParagraphToChapterWithoutDescriptionUnits(String prefix,
-			String newDescriptionUnit) {
+	private void genericUpdateTest(String prefix, String contentToAdd) {
+		genericUpdateTest(prefix, contentToAdd, -1);
+	}
+
+	/**
+	 * <ul>
+	 * <li>Replaces the given prefix by prefix + contentToAdd in the current Document</li>
+	 * <li>Saves the editor</li>
+	 * <li>after merging with the repository content and serializing, check that the editor now displays the
+	 * expected document content.
+	 * <li>
+	 * </ul>
+	 * 
+	 * @param contentToAdd
+	 *            the string corresponding to the new content to add
+	 * @param prefix
+	 *            the prefix to use for identifying the chapter to modify
+	 * @param focusLevel
+	 *            the focus level
+	 */
+	private void genericUpdateTest(String prefix, String contentToAdd, int focusLevel) {
 		// Step 1 : getting the document associated to the opened editor
 		document = (IntentEditorDocument)editor.getDocumentProvider().getDocument(editor.getEditorInput());
 
-		// Step 2 : update section by adding 2 subsections textually
-		String documentContent = document.get();
-		String expectedDocumentContent = documentContent.replace(prefix, prefix + newDescriptionUnit);
+		// Step 2 : update the document
+		// Step 2.1 : including focus level
+		String initialContent = document.get();
+		String indent = LINEBREAK_AND_INDENT;
+		for (int i = 1; i < focusLevel; i++) {
+			indent += "\t";
+		}
+		String updatedPrefix = prefix.replace(LINEBREAK_AND_INDENT, indent);
+		String updatedContentToAdd = contentToAdd.replace(LINEBREAK_AND_INDENT, indent);
+
+		// Step 2.2 : change the document
+		String expectedDocumentContent = initialContent.replace(updatedPrefix, updatedPrefix
+				+ updatedContentToAdd);
 
 		// Step 3 : check merge
 		checkMerging(expectedDocumentContent);
+
 	}
 
 	/**
