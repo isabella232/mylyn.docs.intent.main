15ae48c71d0064b7ac7a84c0f3e68efef02ed93b
wpiers <william.piers@obeo.fr>
2011-10-20 09:43:46 +0200

    applied workaround to activation issues

--- plugins/org.eclipse.mylyn.docs.intent.client.ui.ide/src/org/eclipse/mylyn/docs/intent/client/ui/ide/Activator.java
+++ plugins/org.eclipse.mylyn.docs.intent.client.ui.ide/src/org/eclipse/mylyn/docs/intent/client/ui/ide/Activator.java
@@ -12,6 +12,10 @@ package org.eclipse.mylyn.docs.intent.client.ui.ide;
 
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.mylyn.docs.intent.client.ui.ide.builder.IntentProjectListener;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -51,11 +55,20 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		// Registering a Workspace Listener to handle opening/closing of Intent projects
-		intentProjectListener = new IntentProjectListener();
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		workspace.addResourceChangeListener(intentProjectListener);
 		plugin = this;
+		// Awakes the listener if necessary
+		final Job activateListenerJob = new Job("Activating intent projects listener") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				// Doing activation inside a job workarounds the reentrant calls in the repository
+				// manager.
+				intentProjectListener = new IntentProjectListener();
+				IWorkspace workspace = ResourcesPlugin.getWorkspace();
+				workspace.addResourceChangeListener(intentProjectListener);
+				return Status.OK_STATUS;
+			}
+		};
+		activateListenerJob.schedule();
 	}
 
 	/**
@@ -64,9 +77,9 @@ public class Activator extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		workspace.removeResourceChangeListener(intentProjectListener);
+		plugin = null;
 		super.stop(context);
 	}
 
