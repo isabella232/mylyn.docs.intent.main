3557920f5d893006cffa96f0ec8e95cca7378387
wpiers <william.piers@obeo.fr>
2012-05-23 11:11:46 +0200

    added test

---
+++ tests/org.eclipse.mylyn.docs.intent.client.ui.test/src/org/eclipse/mylyn/docs/intent/client/ui/test/unit/scenario/SimpleCreationTest.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.docs.intent.client.ui.test.unit.scenario;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.mylyn.docs.intent.client.ui.editor.IntentEditor;
+import org.eclipse.mylyn.docs.intent.client.ui.editor.IntentEditorDocument;
+import org.eclipse.mylyn.docs.intent.client.ui.test.util.AbstractIntentUITest;
+
+/**
+ * <p>
+ * Ensures that the most simple creation test works.
+ * </p>
+ * 
+ * @author <a href="mailto:alex.lagarde@obeo.fr">Alex Lagarde</a>
+ */
+public class SimpleCreationTest extends AbstractIntentUITest {
+	private static final String INTENT_DOC_PATH = "data/unit/documents/scenario/empty.intent";
+
+	private IntentEditor editor;
+
+	private IntentEditorDocument document;
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		// Step 1 : Generic set up
+		setUpIntentProject("intentProject", INTENT_DOC_PATH, true);
+
+		// Step 2 : open an editor on the root document
+		editor = openIntentEditor();
+		document = (IntentEditorDocument)editor.getDocumentProvider().getDocument(editor.getEditorInput());
+	}
+
+	/**
+	 * Ensures that abstract resources are not synchronized.
+	 */
+	public void testSimpleModifications() {
+		repositoryListener.startRecording();
+
+		document.set("Document {\n\tChapter Title {\n\t\tText\n\n\t\tSection Title {\n\t\t\tText\n\t\t}\n\t}\n\tChapter Title {\n\t\tText\n\t}\n}");
+		editor.doSave(new NullProgressMonitor());
+		waitForAllOperationsInUIThread();
+		waitForSynchronizer();
+
+		document.set("Document {\n\tChapter C1 {\n\t\tText\n\n\t\tSection C11 {\n\t\t\tText\n\t\t}\n\t}\n\tChapter C2 {\n\t\tText\n\t}\n}");
+		editor.doSave(new NullProgressMonitor());
+		waitForAllOperationsInUIThread();
+		waitForSynchronizer();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		if (editor != null) {
+			editor.close(false);
+		}
+		super.tearDown();
+	}
+}
