82cc61c3970526f1098206fd1683a5bd8037c22e
Alex Lagarde <alex.lagarde@obeo.fr>
2013-03-08 11:56:16 +0100

    Minor UI fixes - Step 2: contributing a new commonSorter to make sure that Intent index entries are sorted according to model order and not alphabetically

---
+++ plugins/org.eclipse.mylyn.docs.intent.client.ui.ide/src/org/eclipse/mylyn/docs/intent/client/ui/ide/navigator/RepositoryContentSorter.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.mylyn.docs.intent.client.ui.ide.navigator;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
+import org.eclipse.mylyn.docs.intent.core.indexer.IntentIndexEntry;
+
+/**
+ * A {@link ViewerSorter} used to display content provided by the {@link RepositoryContentProvider} with the
+ * order defined in model instead of alphabetical order.
+ * 
+ * @author <a href="mailto:alex.lagarde@obeo.fr">Alex Lagarde</a>
+ */
+public class RepositoryContentSorter extends ViewerSorter {
+
+	/**
+	 * Default constructor.
+	 */
+	public RepositoryContentSorter() {
+		super();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.jface.viewers.ViewerComparator#compare(org.eclipse.jface.viewers.Viewer,
+	 *      java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public int compare(Viewer viewer, Object e1, Object e2) {
+		if (e1 instanceof IntentIndexEntry && e2 instanceof IntentIndexEntry) {
+			IntentIndexEntry entry1 = (IntentIndexEntry)e1;
+			IntentIndexEntry entry2 = (IntentIndexEntry)e2;
+			if (entry1.eContainer() == entry2.eContainer()) {
+				String entry1Level = entry1.getName().substring(0, entry1.getName().indexOf(" "));
+				entry1Level = entry1Level.substring(entry1.getName().lastIndexOf((".")) + 1);
+				String entry2Level = entry2.getName().substring(0, entry2.getName().indexOf(" "));
+				entry2Level = entry2Level.substring(entry1.getName().lastIndexOf((".")) + 1);
+				if (entry1Level.length() != entry2Level.length()) {
+					return entry1Level.length() - entry2Level.length();
+				}
+				return entry1Level.compareTo(entry2Level);
+			}
+		}
+		return super.compare(viewer, e1, e2);
+	}
+
+}
